f63a026e8f86a8a73092b847acf2fc00c39394a2
Minzhi <quminzhi@gmail.com>
2023-10-16 13:16:12 -0700

    Added test from homework

--- rail_test.go
+++ rail_test.go
@@ -3,24 +3,52 @@ package rail
 import "testing"
 
 func TestEncode(t *testing.T) {
-	got := Encode("WEAREDISCOVEREDFLEEATONCE", []int{3, 1})
-	if got != "WECRLTEERDSOEEFEAOCAIVDEN" {
-		println("error #1\n\tgot !" + got + "!\n\twant!WECRLTEERDSOEEFEAOCAIVDEN!")
-	}
-	got = Encode("Hello, World!", []int{3, 1})
-	if got != "Hoo!el,Wrdl l" {
-		println("error #2=>" + got)
+	testcases := []struct {
+		name      string
+		plaintext string
+		key       []int
+		expected  string
+	}{
+		{"test0", "WEAREDISCOVEREDFLEEATONCE", []int{3, 1}, "WECRLTEERDSOEEFEAOCAIVDEN"},
+		{"test1", "Hello, World!", []int{3, 1}, "Hoo!el,Wrdl l"},
+		{"test2", "Hello, World!", []int{3, 2}, "Herlo!lWd o,l"},
+		{"test_homework",
+			"CRYPTOLOGY IS THE PRACTICE AND STUDY OF TECHNIQUES FOR SECURE COMMUNICATION IN THE PRESENCE OF THIRD PARTIES" +
+				" CALLED ADVERSARIES",
+			[]int{4, 5},
+			"CT HEGCONAYQNENMDD FRLMUET EIIEE ISTOPCSRT CC FLRSIPVI TO IR  TH  OSFAAHNCOTC  DSRDRN SAIUY CDRTEAPHA CUPEEAEIOEOLSIETNEES YRRU"},
 	}
 
+	for _, e := range testcases {
+		got := Encode(e.plaintext, e.key)
+		if got != e.expected {
+			t.Errorf("Test case %s failed. Expected %v, but got %v\n", e.name, e.expected, got)
+		}
+	}
 }
 
 func TestDecode(t *testing.T) {
-	got := Decode("WECRLTEERDSOEEFEAOCAIVDEN", []int{3, 1})
-	if got != "WEAREDISCOVEREDFLEEATONCE" {
-		println("error #1=>" + got)
+	testcases := []struct {
+		name       string
+		ciphertext string
+		key        []int
+		expected   string
+	}{
+		{"test0", "WECRLTEERDSOEEFEAOCAIVDEN", []int{3, 1}, "WEAREDISCOVEREDFLEEATONCE"},
+		{"test1", "Hoo!el,Wrdl l", []int{3, 1}, "Hello, World!"},
+		{"test2", "Herlo!lWd o,l", []int{3, 2}, "Hello, World!"},
+		{"test_homework",
+			"TPSNIONFRMHANOIREEOIBTSEAKLAPSEHISOBPEGTBRQREPTTMHRTHTTAWE" +
+				"AACTFVAECAOLHANSEEKFHALOITUEEAICNLEYOLTOLEPADFKATATSJMSIINSH" +
+				"ROCTITRIEEAKYNHYUEOTTSTATSIIRSARERUYUEDPCLETEROINEIYEHC",
+			[]int{3, 3},
+			"THERAILFENCECIPHERISANEASYTOAPPLYTRANSPOSITIONCIPHERTHATJUMBLESUPTHEORDEROFTHELETTERSOFAMESSAGEINAQUICKCONVENIENTWAYITALSOHASTHESECURITYOFAKEYTOMAKEITALITTLEBITHARDERTOBREAK"},
 	}
-	got = Decode("Hoo!el,Wrdl l", []int{3, 1})
-	if got != "Hello, World!" {
-		println("error #2=>" + got)
+
+	for _, e := range testcases {
+		got := Decode(e.ciphertext, e.key)
+		if got != e.expected {
+			t.Errorf("Test case %s failed. Expected %v, but got %v\n", e.name, e.expected, got)
+		}
 	}
 }
